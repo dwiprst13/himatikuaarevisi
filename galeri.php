cb92cb57ffe3489138e830f45a1fda8a41ecfd2e
Nurul Fadilah <150462453+Nufa-nur@users.noreply.github.com>
2024-06-11 11:23:25 +0700

    Delete galeri.php

--- galeri.php
+++
@@ -1,9 +0,0 @@
-<section class="w-[100%] mx-auto py-10 bg-gray-900">
-    <div class="w-[95%] mx-auto md:w-[90%] p-3 md:p-5 text-center">
-        <h2 class="text-center font-bold text-[1.6rem] md:text-[2rem] lg:text-[2.5rem] text-blue-700">Galeri</h2>
-        <p class="text-gray-500 font-semibold">galeri belum tersedia</p>
-    </div>
-    <div class="mt-5 flex justify-center">
-        <button type="submit" class="block rounded-md bg-blue-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-700">Lihat Galeri</button>
-    </div>
-</section>
